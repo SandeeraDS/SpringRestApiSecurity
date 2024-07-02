24cc97b1fd2afec713f7e241289102f1576c07db
Dilusha Sandeera Jayampathi <sandeerads@gmail.com>
2024-07-02 22:29:00 +0530

    Spring security with spring supporting tables

--- spring_security_2/src/main/java/org/ds/spring_security_2/security/DemoSecurityConfig.java
+++ spring_security_2/src/main/java/org/ds/spring_security_2/security/DemoSecurityConfig.java
@@ -8,30 +8,39 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails uthpalaH = User.builder()
-                .username("uthpala")
-                .password("{noop}test1234")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-        UserDetails sandeeraJ = User.builder()
-                .username("sandeera")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-        UserDetails nadunD = User.builder()
-                .username("nadun")
-                .password("{noop}test12345")
-                .roles("EMPLOYEE")
-                .build();
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails uthpalaH = User.builder()
+//                .username("uthpala")
+//                .password("{noop}test1234")
+//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+//                .build();
+//        UserDetails sandeeraJ = User.builder()
+//                .username("sandeera")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE", "MANAGER")
+//                .build();
+//        UserDetails nadunD = User.builder()
+//                .username("nadun")
+//                .password("{noop}test12345")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(sandeeraJ, uthpalaH, nadunD);
+//    }
 
-        return new InMemoryUserDetailsManager(sandeeraJ, uthpalaH, nadunD);
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
--- spring_security_2/src/main/resources/Queries.sql
+++ spring_security_2/src/main/resources/Queries.sql
@@ -1,15 +1,53 @@
-CREATE TABLE EMPLOYEE(
-                         ID SERIAL PRIMARY KEY,
-                         FIRST_NAME character varying(50),
-                         LAST_NAME character varying(50),
-                         EMAIL character varying(50)
+CREATE TABLE EMPLOYEE
+(
+    ID         SERIAL PRIMARY KEY,
+    FIRST_NAME character varying(50),
+    LAST_NAME  character varying(50),
+    EMAIL      character varying(50)
 );
 COMMIT;
 
-INSERT INTO EMPLOYEE(FIRST_NAME,LAST_NAME,EMAIL)
-VALUES
-('Sandeera','Jayampathi','sandeera@gmail.com'),
-('Uthpala','Hewage','uthpala@gmail.com'),
-('Nadun','Dhananjaya','nadun@gmail.com');
+INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, EMAIL)
+VALUES ('Sandeera', 'Jayampathi', 'sandeera@gmail.com'),
+       ('Uthpala', 'Hewage', 'uthpala@gmail.com'),
+       ('Nadun', 'Dhananjaya', 'nadun@gmail.com');
 
 COMMIT;
+
+CREATE TABLE IF NOT EXISTS users
+(
+    username character varying(50) NOT NULL,
+    password character varying(50) NOT NULL,
+    enabled  numeric(1, 0)         NOT NULL DEFAULT 1,
+    CONSTRAINT users_pkey PRIMARY KEY (username)
+);
+
+CREATE TABLE IF NOT EXISTS authorities
+(
+    username  character varying(50) NOT NULL,
+    authority character varying(50) NOT NULL,
+    CONSTRAINT username_authority UNIQUE (username, authority),
+    CONSTRAINT fk_username FOREIGN KEY (username)
+        REFERENCES users (username)
+);
+
+insert into authorities
+values ('sandeera', 'ROLE_EMPLOYEE'),
+       ('sandeera', 'ROLE_MANAGER'),
+       ('uthpala', 'ROLE_EMPLOYEE'),
+       ('uthpala', 'ROLE_MANAGER'),
+       ('uthpala', 'ROLE_ADMIN'),
+       ('nadun', 'ROLE_EMPLOYEE');
+
+
+create table users
+(
+    username character varying(50) primary key,
+    password character varying(100) not null,
+    enabled  numeric(1)             not null default 1
+);
+
+insert into users
+values ('uthpala', '{bcrypt}$2a$10$fEokxiEOic2mdtQcUujzfe6gDPMKvhUYohzebcKROSKu6XcxmNM7i', 1),
+       ('sandeera', '{bcrypt}$2a$10$kDtMWg2vuHcvLPrd1/BNL.EX/Rlxt6RZssZI0jBkqU2Amg6jbuVyW', 1),
+       ('nadun', '{bcrypt}$2a$10$kDtMWg2vuHcvLPrd1/BNL.EX/Rlxt6RZssZI0jBkqU2Amg6jbuVyW', 1);
